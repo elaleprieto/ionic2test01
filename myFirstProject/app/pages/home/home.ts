95f3090c91050e517c5d283aabd0f47961871f8b
ale en t64 <aleprieto@gmail.com>
2016-07-15 20:36:29 -0300

    About Page

--- myFirstProject/app/pages/home/home.ts
+++ myFirstProject/app/pages/home/home.ts
@@ -1,37 +1,42 @@
 import {Component} from '@angular/core';
 import {NavController, Alert} from 'ionic-angular';
 import {DateAnnotation} from '../../pipes/mypipe';
+import {AboutPage} from '../about/about';
 
 @Component({
-  templateUrl: 'build/pages/home/home.html',
-  pipes: [DateAnnotation]
+	templateUrl: 'build/pages/home/home.html',
+	pipes: [DateAnnotation]
 })
 export class HomePage {
 
-  currentTime: number;
-  newTime: number;
+	currentTime: number;
+	newTime: number;
 
-  constructor(private navController: NavController) {
-    this.currentTime = new Date().getFullYear();
-  }
+	constructor(private navController: NavController) {
+		this.currentTime = new Date().getFullYear();
+	}
 
-  showAlert() {
-    let prompt = Alert.create({
-      title: 'Alerta!',
-      message: 'Alerta creada!',
-      buttons: [{
-        text: 'Aceptar'
-      }]
-    });
+	about() {
+		this.navController.push(AboutPage);
+	}
 
-    this.navController.present(prompt);
-  }
+	showAlert() {
+		let prompt = Alert.create({
+			title: 'Alerta!',
+			message: 'Alerta creada!',
+			buttons: [{
+				text: 'Aceptar'
+			}]
+		});
 
-  showPast() {
-    this.newTime = this.getRandomInt(1000, 4000);
-  }
+		this.navController.present(prompt);
+	}
 
-  getRandomInt(min: number, max: number) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+	showPast() {
+		this.newTime = this.getRandomInt(1000, 4000);
+	}
+
+	getRandomInt(min: number, max: number) {
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
 }
